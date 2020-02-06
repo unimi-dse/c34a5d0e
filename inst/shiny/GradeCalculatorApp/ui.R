c229f863b41b570275a41277eb5206e89ab8fea3
giorgiamazzi <jo181094@hotmail.it>
2020-02-06 14:57:44 +0100

    Update ui.R

--- inst/shiny/GradeCalculatorApp/ui.R
+++ inst/shiny/GradeCalculatorApp/ui.R
@@ -1,15 +1,16 @@
 ui <- fluidPage(
-  
+
   tags$head(
     tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
   ),
-  
+
   titlePanel(h1("Grade Calculator", align='center')),
-  
+
   sidebarLayout(
-    sidebarPanel("REMARK: Use the name which is in the csv file"),
+    sidebarPanel("REMARK: Use the name which is in the csv file.
+                 For example: use 'Giorgia'"),
     mainPanel(
-      
+
       div(
         h1("Would you like to know your Final Grade Classification?", align='center'),
         textInput("text", h3("Insert your ID name")),
@@ -18,5 +19,5 @@ ui <- fluidPage(
       )
     )
   )
-  
+
 )
